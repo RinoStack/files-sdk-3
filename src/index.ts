a12d5c4a93c7c8ba00cc09442b3aaafa3a8d1324
RinoStack <rino44296@gmail.com>
2026-06-27 08:24:56 +0300

    chore: add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export class FilesSDK {
+  constructor(config) {
+    this.config = config;
+  }
+
+  public async upload(file: File) {
+    // Upload to any backend
+  }
+
+  public async download(key: string) {
+    // Download from any backend
+  }
+}
